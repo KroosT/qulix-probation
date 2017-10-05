ef16f88863656fca160f5f7c3893f9e964968410
KroosT <anton.shestak.96@gmail.com>
2017-10-05 09:50:17 +0300

    Added class with static method counting edges of the figures types of the Figure2 enum.

---
+++ 1. Enums/TestEnum.java
@@ -0,0 +1,16 @@
+class TestEnum {
+
+    static int edgesCount(Figure2 f) {
+        switch (f) {
+            case CIRCLE:
+                return Integer.MAX_VALUE;
+            case SQUARE:
+                return 4;
+            case TRIANGLE:
+                return 3;
+            default:
+                return -1;
+        }
+    }
+    
+}
