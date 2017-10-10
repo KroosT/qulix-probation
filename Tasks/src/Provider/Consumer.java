502e973960ab62eb96b2918b04231846b0ad840a
KroosT <anton.shestak.96@gmail.com>
2017-10-10 16:58:31 +0300

    Modified consumer's method to allow passing list of inheritors of A class.

--- Tasks/src/Provider/Consumer.java
+++ Tasks/src/Provider/Consumer.java
@@ -3,7 +3,7 @@ package Provider;
 import java.util.ArrayList;
 
 class Consumer {
-    public static void consume(final ArrayList<A> as) {
+    public static void consume(final ArrayList<? extends A> as) {
         // actual processing is not relied to problem
     }
 }
