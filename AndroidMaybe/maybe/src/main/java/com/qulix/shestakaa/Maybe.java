f24d46c2eae6fa2bed64b9a3f1646edda9fa8481
KroosT <anton.shestak.96@gmail.com>
2017-10-11 09:58:22 +0300

    Maybe class complited. Added validator util.

--- AndroidMaybe/maybe/src/main/java/com/qulix/shestakaa/Maybe.java
+++ AndroidMaybe/maybe/src/main/java/com/qulix/shestakaa/Maybe.java
@@ -1,4 +1,38 @@
 package com.qulix.shestakaa;
 
-public class Maybe {
+import com.qulix.shestakaa.Utils.Validator;
+import java.util.NoSuchElementException;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+class Maybe<T> {
+
+    @Nullable
+    private final T mValue;
+
+    private Maybe(@Nullable final T value) {
+        mValue = value;
+    }
+
+    public static <T> Maybe<T> just(final T value) {
+        Validator.isArgNotNull(value, "value");
+        return new Maybe<>(value);
+    }
+
+    public static <T> Maybe<T> empty() {
+        return new Maybe<>((T) null);
+    }
+
+    public T getValueOrThrow() {
+        if (mValue == null) {
+            throw new NoSuchElementException("No such value.");
+        }
+        return mValue;
+    }
+
+    public boolean isDefined() {
+        return mValue != null;
+    }
 }
