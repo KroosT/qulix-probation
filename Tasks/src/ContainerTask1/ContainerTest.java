07e4ec65d2d75bd3113bb78ddcc795768339b71d
KroosT <anton.shestak.96@gmail.com>
2017-10-13 11:33:30 +0300

    Modified tests.
    Container instance recreated before each test;
    Adding elements to container moved into tests methods.
    Added test method that actually tests that copying constructor works
    as expected.
    Used AssertJ.

--- Tasks/src/ContainerTask1/ContainerTest.java
+++ Tasks/src/ContainerTask1/ContainerTest.java
@@ -1,37 +1,55 @@
 package ContainerTask1;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class ContainerTest {
 
-    private final Container<Integer> mContainerOfIntegers = new Container<>();
+    private Container<Integer> mContainerOfIntegers;
 
     @Before
     public void init() {
+        mContainerOfIntegers = new Container<>();
+    }
+
+    @Test
+    public void testRemoveAtCorrectlyRemovesObjectByPassedIndex() throws Exception {
         mContainerOfIntegers.add(30);
         mContainerOfIntegers.add(28);
         mContainerOfIntegers.add(1);
         mContainerOfIntegers.add(28);
+
+        mContainerOfIntegers.removeAt(1);
+        final List<Integer> expected = Arrays.asList(30, 1, 28);
+
+        assertThat(mContainerOfIntegers.getList()).isEqualTo(expected);
+    }
+
+    @Test
+    public void testRemoveObjectCorrectlyRemovesPassedObject() throws Exception {
         mContainerOfIntegers.add(30);
         mContainerOfIntegers.add(28);
         mContainerOfIntegers.add(1);
+
+        mContainerOfIntegers.remove(28);
+        final List<Integer> expected = Arrays.asList(30, 1);
+
+        assertThat(mContainerOfIntegers.getList()).isEqualTo(expected);
     }
 
     @Test
-    public void testRemoveObjectByIndex() throws Exception {
-        final Integer removedItem = mContainerOfIntegers.removeAt(1);
-        Assert.assertFalse("Container must not contain removed item any more",
-                mContainerOfIntegers.getList().get(1).equals(removedItem));
-    }
+    public void testCopyConstructorReturnsNotTheSameObject() throws Exception {
+        mContainerOfIntegers.add(30);
+        mContainerOfIntegers.add(28);
+        mContainerOfIntegers.add(1);
 
-    @Test
-    public void testRemoveObject() throws Exception {
-        final Integer value = 28;
-        mContainerOfIntegers.remove(value);
-        Assert.assertFalse("Container must not contain removed item any more",
-                mContainerOfIntegers.getList().contains(value));
-    }
+        final Container<Integer> copy = new Container<>(mContainerOfIntegers);
 
+        assertThat(copy.getList()).isNotSameAs(mContainerOfIntegers.getList());
+    }
 }
