faeaa120e8c869ac13a8e6786494cb35e27a021b
KroosT <anton.shestak.96@gmail.com>
2017-10-13 12:14:30 +0300

    Improved tests by using AssertJ.

--- Tasks/src/ContainerTask3/ContainerTest.java
+++ Tasks/src/ContainerTask3/ContainerTest.java
@@ -1,42 +1,68 @@
 package ContainerTask3;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 import static ContainerTask3.RealtekProductType.AUDIO;
 import static ContainerTask3.RealtekProductType.NETWORK;
 
 public class ContainerTest {
+
+
+
     @Test
     public void testContainerFilterByStringParameter() throws Exception {
-        final Container<Realtek, String> realtekContainer = new Container<>();
 
-        realtekContainer.add(new Realtek("Audio"));
-        realtekContainer.add(new Realtek("Audio"));
-        realtekContainer.add(new Realtek("Audio"));
-        realtekContainer.add(new Realtek("Network"));
-        realtekContainer.add(new Realtek("Network"));
+        final Container<Realtek, String> mContainer = new Container<>();
 
-        final Container<Realtek, String> realtekContainerAnswersTo = realtekContainer
+        final Realtek realtek1 = new Realtek("AUDIO");
+        final Realtek realtek2 = new Realtek("AUDIO");
+        final Realtek realtek3 = new Realtek("NETWORK");
+        final Realtek realtek4 = new Realtek("NETWORK");
+        final Realtek realtek5 = new Realtek("NETWORK");
+
+        mContainer.add(realtek1);
+        mContainer.add(realtek2);
+        mContainer.add(realtek3);
+        mContainer.add(realtek4);
+        mContainer.add(realtek5);
+
+        final Container<Realtek, String> containerAnswersTo = mContainer
                 .allThatAnswersTo("AUDIO");
+        final List<Realtek> expected = Arrays.asList(realtek1, realtek2);
 
-        Assert.assertEquals(realtekContainerAnswersTo.getList().size(), 3);
+        assertThat(containerAnswersTo.getList()).isEqualTo(expected);
+
     }
 
     @Test
     public void testContainerFilterByEnumParameter() throws Exception {
-        final Container<RealtekNew, RealtekProductType> realtekNewContainer = new Container<>();
 
-        realtekNewContainer.add(new RealtekNew(AUDIO));
-        realtekNewContainer.add(new RealtekNew(AUDIO));
-        realtekNewContainer.add(new RealtekNew(AUDIO));
-        realtekNewContainer.add(new RealtekNew(NETWORK));
-        realtekNewContainer.add(new RealtekNew(NETWORK));
+        final Container<RealtekNew, RealtekProductType> container = new Container<>();
 
-        final Container<RealtekNew, RealtekProductType> realtek2ContainerAnswersTo =
-                realtekNewContainer.allThatAnswersTo(NETWORK);
+        final RealtekNew realtek1 = new RealtekNew(AUDIO);
+        final RealtekNew realtek2 = new RealtekNew(AUDIO);
+        final RealtekNew realtek3 = new RealtekNew(NETWORK);
+        final RealtekNew realtek4 = new RealtekNew(NETWORK);
+        final RealtekNew realtek5 = new RealtekNew(NETWORK);
 
-        Assert.assertEquals(realtek2ContainerAnswersTo.getList().size(), 2);
+        container.add(realtek1);
+        container.add(realtek2);
+        container.add(realtek3);
+        container.add(realtek4);
+        container.add(realtek5);
+
+        final Container<RealtekNew, RealtekProductType> containerAnswersTo =
+                container.allThatAnswersTo(NETWORK);
+
+        final List<RealtekNew> expected = Arrays.asList(realtek3, realtek4, realtek5);
+
+        assertThat(containerAnswersTo.getList()).isEqualTo(expected);
 
 
     }
